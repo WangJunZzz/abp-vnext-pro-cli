b3a42cd9c6044db58ebb3b2feb491b4d03a90856
WangJunZzz <510423039@qq.com>
2022-08-11 23:23:21 +0800

    ✨ 移除版本检查

--- src/Lion.AbpPro.Cli.Core/Lion/AbpPro/Cli/CliService.cs
+++ src/Lion.AbpPro.Cli.Core/Lion/AbpPro/Cli/CliService.cs
@@ -34,7 +34,7 @@ public class CliService : DomainService
         Console.WriteLine("Lion ABP Pro CLI (https://https://doc.cncore.club/)");
         Console.WriteLine("请输入 lion.abp help 查看所有命令");
         
-        await _checkManager.CheckCliVersionAsync(LionAbpProCliConsts.PackageId);
+        //await _checkManager.CheckCliVersionAsync(LionAbpProCliConsts.PackageId);
 
         try
         {
